7c8ec35ad783ce2fc85e37fc1aa546400168f636
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2022-11-29 20:56:03 +0100

    To class Router add methods to add and match routes in the routing table

--- Core/Router.php
+++ Core/Router.php
@@ -2,7 +2,93 @@
 
 namespace Core;
 
+/**
+ * Router
+ */
+
 class Router
 {
+    /**
+     * Associative array of routes
+     * 
+     * @var array
+     */
+    protected $routes = [];
 
+    /**
+     * Parameters from the matched route
+     * 
+     * @var array
+     */
+    protected $parameters = [];
+
+    /**
+     * Add a route to the routing table
+     * 
+     * @param string $route     The route URL
+     * @param array $parameters Parameters
+     * 
+     * @return void 
+     */
+    public function add($route, $parameters = [])
+    {
+        // Convert the route to a regular expression: escape forward slashes
+        $route = preg_replace('/\//', '\\/', $route);
+
+        // Convert variables e.g. {controller}
+        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+
+        // Convert variables with custom regular expressions e.g. {id:\d+}
+        $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
+
+        // Add start and end delimiters, and case insensitive flag
+        $route = '/^' . $route . '$/i';
+
+        $this->routes[$route] = $parameters;
+    }
+
+    /**
+     * Get all routes from the routing table
+     * 
+     * @return array
+     */
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
+
+    /**
+     * Match the route to the routes in the routing table, setting the $parameters
+     * property if the route is found
+     * 
+     * @param string $url   The route URL
+     * 
+     * @param boolean true if a match found, false otherwise
+     */
+    public function match($url)
+    {
+        foreach ($this->routes as $route => $parameters) {
+            if (preg_match($route, $url, $matches)) {
+                // Get named capture group values
+                foreach ($matches as $key => $match) {
+                    if (is_string($key)) {
+                        $parameters[$key] = $match;
+                    }
+                }
+                $this->parameters = $parameters;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Get the matched parameters
+     * 
+     * @return array
+     */
+    public function getParameters()
+    {
+        return $this->parameters;
+    }
 }
--- public/index.php
+++ public/index.php
@@ -11,4 +11,16 @@ require '../Core/Router.php';
 
 $router = new Core\Router();
 
-echo get_class($router);
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->match($url)) {
+    echo '<pre>';
+    var_dump($router->getParameters());
+    echo '</pre>';
+} else {
+    echo "No route found for URL '$url'";
+}
