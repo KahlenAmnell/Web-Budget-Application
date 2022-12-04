94b1875171fe0f515894dcdaddf233aea89f4c58
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2022-12-04 20:31:17 +0100

    Add class Error to Core to handle errors and exceptions and implement it to the project

--- Core/Controller.php
+++ Core/Controller.php
@@ -47,7 +47,7 @@ namespace Core;
                 $this->after();
             }
         } else {
-            echo "Method $method not found in controller " . get_class($this);
+            throw new \Exception("Method $method not found in controller " . get_class($this));
         }
     }
 
