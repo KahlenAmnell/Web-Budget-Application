0203a157b9a34ab91771408a50f0e2e041f73c8f
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2022-12-18 21:46:17 +0100

    Add class Flash with methods to add flash messages and get it

---
+++ App/Flash.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+/**
+ * Flash messages
+ */
+class Flash
+{
+    /**
+     * Add a message
+     * 
+     * @param string $message The message content
+     * 
+     * @return void
+     */
+    public static function addMessage($message, $type = 'success')
+    {
+        // Create array in the session if it doesn't already exist
+        if (!isset($_SESSION['flash_notifications'])) {
+            $_SESSION['flash_notifications'] = [];
+        }
+
+        // Append the message to the array
+        $_SESSION['flash_notifications'][] = [
+            'body' => $message,
+            'type' => $type
+        ];
+    }
+
+    /**
+     * Get all the messages
+     * 
+     * @return mixed An array with all the messages or null if none set
+     */
+    public static function getMessages()
+    {
+        if (isset($_SESSION['flash_notifications'])) {
+            $messages = $_SESSION['flash_notifications'];
+            unset($_SESSION['flash_notifications']);
+            return $messages;
+        }
+    }
+}
