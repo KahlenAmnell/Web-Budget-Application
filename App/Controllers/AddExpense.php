687ffef40db9d0ff5023202e30d4e2badd5cdd27
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2022-12-28 21:05:54 +0100

    Add AddExpense controller class

---
+++ App/Controllers/AddExpense.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+use \App\Models\Expense;
+use \App\Flash;
+use \App\Auth;
+
+/**
+ * Add expense controller
+ */
+class AddExpense extends Authenticated
+{
+    /**
+     * Show add income page
+     * 
+     * @return void
+     */
+    public function indexAction()
+    {
+    }
+}
