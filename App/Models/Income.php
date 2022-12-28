a24140b0520ec4adf34ca14c1b122ccc4d06637c
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2022-12-28 20:56:30 +0100

    add methods to validate income data and save it to database

--- App/Models/Income.php
+++ App/Models/Income.php
@@ -31,7 +31,9 @@ class Income extends \Core\Model
     }
 
     /**
+     * Get categories assign to users
      * 
+     * @return array Array with income categories of logged in user
      */
     public static function getIncomeCategories()
     {
@@ -58,6 +60,52 @@ class Income extends \Core\Model
      */
     public function save()
     {
-        return true;
+        $this->validate();
+
+        if (empty($this->errors)) {
+            $sql = 'INSERT INTO incomes (userID, incomeCategoryAssignedToUserId, amount, dateOfIncome, incomeComment)
+                    VALUES (:id, :category, :amount, :date, :comment)';
+
+            $db = static::getDB();
+
+            $stmt = $db->prepare($sql);
+            $stmt->bindValue(':id', $_SESSION['user_id'], PDO::PARAM_INT);
+            $stmt->bindValue(':category', $this->category, PDO::PARAM_INT);
+            $stmt->bindValue(':amount', $this->amount, PDO::PARAM_STR);
+            $stmt->bindValue(':date', $this->date, PDO::PARAM_STR);
+            $stmt->bindValue(':comment', $this->comment, PDO::PARAM_STR);
+
+            return $stmt->execute();
+        }
+        return false;
+    }
+
+    /**
+     * Validate current data, adding validation errors messages to the errors array property
+     * 
+     * @return void
+     */
+    public function validate()
+    {
+        //amount
+        if ($this->amount <= 0) {
+            $this->errors[] = 'Kwota musi być większa niż 0.';
+        }
+        if ($this->amount != round($this->amount, 2)) {
+            $this->errors[] = 'Kwota musi być zaokrąglona do dwóch miejsc po przecinku.';
+        }
+
+        //data
+        if ($this->date > date("Y-m-d")) {
+            $this->errors[] = 'Data nie może być przyszła.';
+        }
+        if ($this->date < "2000-01-01") {
+            $this->errors[] = 'Data nie może wcześniejsza niż 01-01-2000.';
+        }
+
+        //category
+        if ($this->category == '') {
+            $this->errors[] = 'Musisz podać kategorię przychodu';
+        }
     }
 }
