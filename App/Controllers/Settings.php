0fae85860f7792743a31aced83c6063d7692ce82
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2023-02-02 12:19:32 +0100

    Add form and methods to update user profile

--- App/Controllers/Settings.php
+++ App/Controllers/Settings.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 use \Core\View;
+use \App\Auth;
+use \App\Flash;
 
 /**
  * Settings controller
@@ -16,6 +18,22 @@ class Settings extends Authenticated
 
     public function editProfileAction()
     {
-        View::renderTemplate('Settings/editProfile.html');
+        View::renderTemplate('Settings/editProfile.html', [
+            'user' => Auth::getUser()
+        ]);
+    }
+
+    public function updateAction()
+    {
+        $user = Auth::getUser();
+        var_dump('tu');
+        if($user->updateProfile($_POST)){
+            Flash::addMessage('Zapisano zmiany');
+            $this->redirect('/settings/index');
+        } else {
+            View::renderTemplate('Settings/editProfile.html', [
+                'user' => $user
+            ]);
+        }
     }
 }
